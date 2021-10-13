8a51863f280810fd113b0e4c33075037080d615d
Fernando Mireles <fernandodilland@gmail.com>
2021-10-13 14:54:53 -0500

    Update Evidencia1.py

--- Evidencia1.py
+++ Evidencia1.py
@@ -1,2 +1,23 @@
-# Fernando Dilland Mireles Cisneros.
-# Andrea Aracely Cantú Cisneros.
+"""
+Evidencia #1, Estructura de datos y su procesamiento.
+
+Fernando Dilland Mireles Cisneros.
+ Andrea Aracely Cantú Cisneros. """
+
+
+from collections import namedtuple
+
+print("Bienvenido(a) al negocio de ventas de llantas")
+
+Personas = namedtuple("Personas",["nombre","correoElectronico"])
+DiccionarioPersonas = {}
+
+while True:
+    
+    opcion = int(input("Menú de opciones:\n1)Registrar una venta\n2)Consultar una venta\n3)Salir\n> "))
+    
+    
+
+    if opcion == 3: # Salida del menú
+        break
+    
